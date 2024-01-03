391deaa0712e3209381a8606f82189e87e99c9d7
Geovane <geovaneviana.df@gmail.com>
2024-01-02 21:03:11 -0300

    mais um commit

---
+++ 10-atividade/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _10Atividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+
+            while(cont < 2)
+            {
+            double nota1, nota2, media;
+
+            Console.WriteLine("Digite a primeira nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Digite a segunda nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado!!!!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado!!!");
+            }
+            Console.WriteLine(media);
+            cont++;
+            }
+        }
+    }
+}
---
+++ 11-atividade/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _11Atividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numConta, saldo, deb, cred;
+            double calSaldo;
+            int cont = 0;
+
+            while(cont < 2)
+            {
+
+            Console.WriteLine("Digite numero da conta: ");
+            numConta = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Saldo: ");
+            saldo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Débito:");
+            deb = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Crédito:");
+            cred = Convert.ToDouble(Console.ReadLine());
+
+            calSaldo = saldo - deb - cred;
+
+            saldo = calSaldo;
+
+            if(saldo >= 0)
+            {
+                Console.WriteLine("Saldo Positivo");
+            }
+            else 
+            {
+                Console.WriteLine("Saldo Negativo");
+            }
+            Console.WriteLine(saldo);
+            cont++;
+            }
+        }
+    }
+}
